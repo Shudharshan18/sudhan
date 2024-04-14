54dc35e2d3a3188da224a6c4fb83321dc806bb7a
Shudharshan18 <161675949+Shudharshan18@users.noreply.github.com>
2024-04-14 19:53:54 +0530

    Update settings.py

--- sudhan/settings.py
+++ sudhan/settings.py
@@ -87,17 +87,23 @@ WSGI_APPLICATION = 'sudhan.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.postgresql',
-       'NAME': 'verceldb',
-       'USER': 'default',
-       'PASSWORD': '3eME4rjJYNlG',
-       'HOST': 'ep-dark-wood-a4y2ur1d-pooler.us-east-1.aws.neon.tech',
-       'PORT': '5432',
-   }
-}
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'verceldb',
+#        'USER': 'default',
+#        'PASSWORD': '3eME4rjJYNlG',
+#        'HOST': 'ep-dark-wood-a4y2ur1d-pooler.us-east-1.aws.neon.tech',
+#        'PORT': '5432',
+#    }
+# }
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators

26485b212e594b4d9041961b2bd2e95e89c0e7ae
Shudharshan18 <jshudarshan@gmail.com>
2024-04-15 21:32:24 +0530

    s

--- sudhan/settings.py
+++ sudhan/settings.py
@@ -87,23 +87,23 @@ WSGI_APPLICATION = 'sudhan.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.postgresql',
-#        'NAME': 'verceldb',
-#        'USER': 'default',
-#        'PASSWORD': '3eME4rjJYNlG',
-#        'HOST': 'ep-dark-wood-a4y2ur1d-pooler.us-east-1.aws.neon.tech',
-#        'PORT': '5432',
-#    }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'defaultdb',
+        'USER': 'avnadmin',
+        'PASSWORD': 'AVNS_Jdl5TFq8HUo5Vvl446T',
+        'HOST': 'sudhan-jshudarshan-2437.b.aivencloud.com',   # Or an IP Address that your DB is hosted on
+        'PORT': '14199',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators

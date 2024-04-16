63a4c548712ee297ccfd4ddc6c0ed2c1eec0e866
Shudharshan18 <jshudarshan@gmail.com>
2024-04-16 09:04:46 +0530

    changing name

--- sudhan/urls.py
+++ sudhan/urls.py
@@ -21,6 +21,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('first.urls'))
+    path('',include('movie.urls'))
 ]
 urlpatterns+= static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
